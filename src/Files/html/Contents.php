e79cb49c9dd0a1530befc39d343ad9fc3867fcc7
rmanila <r.manila.513779@umindanao.edu.ph>
2024-06-21 14:09:25 +0800

    MAIN

---
+++ src/Files/html/Contents.php
@@ -0,0 +1,61 @@
+<div class="container">
+        <section class="technology-section">
+            <h2>Web Development</h2>
+            
+            <article class="technology">
+                <h3>JavaScript</h3>
+                <p class="description">JavaScript is a versatile programming language used for web development.</p>
+                <div class="pictures">
+                    <img src="../../../public/js/1.png" alt="JavaScript Picture 1" width ="350px">
+                    <img src="../../../public/js/2.png" alt="JavaScript Picture 2" width ="350px">
+                    <img src="../../../public/js/3.png" alt="JavaScript Picture 3" width ="350px">
+                    <img src="../../../public/js/4.png" alt="JavaScript Picture 4" width ="350px">
+                    <img src="../../../public/js/5.png" alt="JavaScript Picture 5" width ="350px">
+                </div>
+            </article>
+
+            <article class="technology">
+                <h3>Native PHP</h3>
+                <p class="description">Native PHP development involves server-side scripting for web applications.</p>
+                <div class="pictures">
+                    <img src="../../../public/php/1.png" alt="PHP Picture 1" width ="350px">
+                    <img src="../../../public/php/2.png" alt="PHP Picture 2" width ="350px">
+                    <img src="../../../public/php/3.png" alt="PHP Picture 3" width ="350px">
+                    <img src="../../../public/php/4.png" alt="PHP Picture 4" width ="350px"> 
+                </div>
+            </article>
+
+            <article class="technology">
+                <h3>React</h3>
+                <p class="description">React is a JavaScript library for building user interfaces.</p>
+                <div class="pictures">
+                    <img src="../../../public/react/1.png" alt="React Picture 1" width ="350px">
+                    <img src="../../../public/react/2.png" alt="React Picture 2" width ="350px">
+                </div>
+            </article>
+
+            <article class="technology">
+                <h3>Laravel</h3>
+                <p class="description">Laravel is a PHP framework for web development, known for its elegant syntax.</p>
+                <div class="pictures">
+                    <img src="../../../public/laravel/1.png" alt="Laravel Picture 1" width ="350px">
+                    <img src="../../../public/laravel/2.png" alt="Laravel Picture 2" width ="350px">
+                    <img src="../../../public/laravel/3.png" alt="Laravel Picture 3" width ="350px" height= "350px">
+                    <img src="../../../public/laravel/4.png" alt="Laravel Picture 4" width ="350px">
+                    <img src="../../../public/laravel/5.png" alt="Laravel Picture 5" width ="350px">
+                </div>
+            </article>
+        </section>
+<br><br><br><br><br>
+        <section class="technology-section">
+            <h2>Database</h2>
+            
+            <article class="technology">
+                <h3>MySQL/PHPMyAdmin</h3>
+                <p class="description">MySQL is an open-source relational database management system, commonly used with PHPMyAdmin for database administration.</p>
+                <div class="pictures">
+                    <img src="../../../public/database/1.png" alt="MySQL Picture 1">
+                </div>
+            </article>
+        </section>
+    </div>
